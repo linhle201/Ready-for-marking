9ac9908b4aac9f94a6f7b06c0209283fa68cfa06
Linh Le <lethimylinh.2001@gmail.com>
2023-09-20 09:40:52 -0300

    updated caculated

--- program1.js
+++ program1.js
@@ -1,6 +1,6 @@
 "use strict";
 /*Author: Thi My Linh Le
-Date: 9/15/2023
+Date: 9/20/2023
 Description: Assignment1 - program 1
 Hipster’s Local Vinyl Records sell and hand-deliver vinyl records to their customers. Delivery is charged at a rate of $15 per kilometer. 
 All purchases are subject to a 14% sales tax. 
@@ -26,8 +26,8 @@ function main()
     //output
     console.log("Purchase summary for" + nameCustomer +":");
     console.log("Delivery Cost:" +"$" + deliverycost.tofixed(2));
-    console.log("Purchase Cost :" + "$" + purchasecost.tofixed(2) ):
-    console.log("Total Cost:" + "$" + totalcost.tofixed(2))
+    console.log("Purchase Cost :" + "$" + purchasecost.tofixed(2) );
+    console.log("Total Cost:" + "$" + totalcost.tofixed(2));
     
 
     
